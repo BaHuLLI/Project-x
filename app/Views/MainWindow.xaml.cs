6d60af359267a71858ec4b9f39a33468a111e53a
BaHuLLI <103258902+BaHuLLI@users.noreply.github.com>
2026-04-11 00:21:08 +0200

    Rebuild sim racing shell layout

--- app/Views/MainWindow.xaml.cs
+++ app/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Windows.Threading;
 
 namespace ProjectXProDash.Views;
 
@@ -7,5 +8,21 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        Loaded += OnLoaded;
+    }
+
+    private void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        Loaded -= OnLoaded;
+
+        Activate();
+        Topmost = true;
+
+        Dispatcher.BeginInvoke(() =>
+        {
+            Topmost = false;
+            Activate();
+            Focus();
+        }, DispatcherPriority.ApplicationIdle);
     }
 }
